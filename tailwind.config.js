09cd693cb4844a16041ccfff3e894fa64c51e6e3
danillyamkin <danilwebdev@gmail.com>
2022-02-10 08:52:36 +0300

    Configured colors and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'black': '#0D0A19',
+      'orange': '#EF8354',
+      'purple': '#7963F0',
+      'pink': '#FBB0BF',
+      'mint': '#A2F3D9',
+      'gray-black': '#9A9EA7',
+      'gray-white': '#F5F6F7',
+      'gray-light': '#E4E6E8',
+      'gray-medium': '#C8CCD4',
+    },
+    fontFamily: {
+      sans: ['Inter', 'sans-serif']
+    },
     extend: {},
   },
   plugins: [],
