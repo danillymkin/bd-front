f011d5e69477bac10375ec10133531b7583032d5
danillyamkin <danilwebdev@gmail.com>
2022-02-10 13:49:09 +0300

    Added colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,25 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     colors: {
-      'black': '#0D0A19',
-      'orange': '#EF8354',
-      'purple': '#7963F0',
-      'pink': '#FBB0BF',
-      'mint': '#A2F3D9',
+      white: '#FFFFFF',
+      black: '#0D0A19',
+      orange: '#EF8354',
+      purple: '#7963F0',
+      pink: '#FBB0BF',
+      mint: '#A2F3D9',
       'gray-black': '#9A9EA7',
       'gray-white': '#F5F6F7',
       'gray-light': '#E4E6E8',
       'gray-medium': '#C8CCD4',
     },
     fontFamily: {
-      sans: ['Inter', 'sans-serif']
+      sans: ['Inter', 'sans-serif'],
     },
     extend: {},
   },
   plugins: [],
-}
+};
