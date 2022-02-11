1c0e304615006efea2aef1aaea3c26d94fb97d85
danillyamkin <danilwebdev@gmail.com>
2022-02-11 14:01:03 +0300

    Created routes constants

---
+++ constants/routes.ts
@@ -0,0 +1,6 @@
+export const MAIN_ROUTE = '/';
+export const LOGIN_ROUTE = '/login';
+export const CARS_ROUTE = '/cars';
+export const MANUFACTURERS_ROUTE = '/manufacturers';
+export const REPORTS_ROUTE = '/reports';
+export const CONTRACT_ROUTE = '/contract';
