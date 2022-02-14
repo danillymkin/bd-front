6a086a07c743595e84bb0e6e43ac191557c11f55
danillyamkin <danilwebdev@gmail.com>
2022-02-14 18:06:12 +0300

    Added redux wrapper

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
-import '../styles/globals.scss'
-import type { AppProps } from 'next/app'
+import '../styles/globals.scss';
+import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { wrapper } from '../store';
+
+function App({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default wrapper.withRedux(App);
