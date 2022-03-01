f6d9eae0a64499e0b88ef763a7fef2ba27265ab5
danillyamkin <danilwebdev@gmail.com>
2022-03-02 02:11:35 +0300

    Created filter elements

---
+++ components/ui/Filter/FilterVolume/FilterVolume.tsx
@@ -0,0 +1,39 @@
+import React, { FunctionComponent, useState } from 'react';
+
+import classes from './FilterVolume.module.scss';
+import Input from '../../Input/Input';
+
+const FilterVolume: FunctionComponent = (): JSX.Element => {
+  const [volumeFrom, setVolumeFrom] = useState<string>('');
+  const [volumeTo, setVolumeTo] = useState<string>('');
+
+  const onChangeVolumeFrom = (value: string): void => {
+    setVolumeFrom(value);
+  };
+
+  const onChangeVolumeTo = (value: string): void => {
+    setVolumeTo(value);
+  };
+
+  return (
+    <div className={classes.filter}>
+      <Input
+        value={volumeFrom}
+        type={'number'}
+        onChange={onChangeVolumeFrom}
+        min={0}
+        placeholder="Объем, от"
+      />
+
+      <Input
+        value={volumeTo}
+        type={'number'}
+        onChange={onChangeVolumeTo}
+        min={0}
+        placeholder="Объем, до"
+      />
+    </div>
+  );
+};
+
+export default FilterVolume;
