774189ec034d0d92def4bea87ba10dfd49ee5278
danillyamkin <danilwebdev@gmail.com>
2022-03-03 03:08:23 +0300

    Added validate errors

--- http/index.ts
+++ http/index.ts
@@ -5,6 +5,9 @@ export const API_URL = 'http://localhost:5000/api';
 const $api = axios.create({
   withCredentials: true,
   baseURL: API_URL,
+  validateStatus: (status: number) => {
+    return status < 500;
+  },
 });
 
 export default $api;
