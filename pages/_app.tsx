87b32ebfc98c25032e943971f99612c57027d9f5
danillyamkin <danilwebdev@gmail.com>
2022-03-04 02:15:32 +0300

    Added swiper styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
-import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
 
 import { wrapper } from '../store';
+
+import '../styles/globals.scss';
+import 'swiper/scss';
+import 'swiper/scss/navigation';
+import 'swiper/scss/effect-creative';
 
 function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
