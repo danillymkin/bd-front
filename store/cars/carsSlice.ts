23b4dfbba93241a798dd555be1a314b50b818136
danillyamkin <danilwebdev@gmail.com>
2022-03-03 03:12:29 +0300

    Created fetchCarById and added errors handling

--- store/cars/carsSlice.ts
+++ store/cars/carsSlice.ts
@@ -20,10 +20,21 @@ export const fetchAllCars = createAsyncThunk<
 >('cars/fetchAllCars', async (_, thunkApi) => {
   const response = await CarsService.getAll();
   if (response.status !== 200) {
-    console.log('error', response);
-    thunkApi.rejectWithValue(response.data as ServerError);
+    return thunkApi.rejectWithValue(response.data as ServerError);
   }
   return response.data as AllAndCount<Car>;
+});
+
+export const fetchCarById = createAsyncThunk<
+  Car,
+  number | string,
+  { rejectValue: ServerError }
+>('cars/fetchCarById', async (id: number | string, thunkApi) => {
+  const response = await CarsService.getById(id);
+  if (response.status !== 200) {
+    return thunkApi.rejectWithValue(response.data as ServerError);
+  }
+  return response.data as Car;
 });
 
 export const carsSlice = createSlice({
@@ -43,8 +54,7 @@ export const carsSlice = createSlice({
     builder.addCase(HYDRATE, (state: CarsState, action: any) => {
       return {
         ...state,
-        cars: action.payload.cars.cars,
-        totalCars: action.payload.cars.totalCars,
+        ...action.payload.cars,
       };
     });
 
@@ -63,10 +73,46 @@ export const carsSlice = createSlice({
     builder.addCase(fetchAllCars.rejected, (state: CarsState, action) => {
       state.loading = false;
       if (action.payload) {
-        state.error = action.payload.error;
+        state.error = {
+          error: action.payload.error,
+          statusCode: action.payload.statusCode,
+          messages: action.payload.messages,
+        };
       } else {
-        state.error =
-          action.error.message || 'Не удалось получить список автомобилей';
+        state.error = {
+          messages: ['Не удалось получить список автомобилей'],
+          error: action.error.message || 'Bad Request',
+          statusCode: 400,
+        };
+      }
+    });
+
+    builder.addCase(fetchCarById.pending, (state: CarsState) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      fetchCarById.fulfilled,
+      (state: CarsState, action: PayloadAction<Car>) => {
+        state.loading = false;
+        state.cars = [action.payload];
+        state.totalCars = 1;
+      },
+    );
+    builder.addCase(fetchCarById.rejected, (state: CarsState, action) => {
+      state.loading = false;
+      if (action.payload) {
+        state.error = {
+          error: action.payload.error,
+          messages: action.payload.messages,
+          statusCode: action.payload.statusCode,
+        };
+      } else {
+        state.error = {
+          error: action.error.message || 'Bad Request',
+          statusCode: 400,
+          messages: ['Не удалось получить автомобиль'],
+        };
       }
     });
   },
