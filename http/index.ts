b966d73c2db582f343eafc16aedc843bddd47b41
danillyamkin <danilwebdev@gmail.com>
2022-03-05 01:56:10 +0300

    Added static domain

--- http/index.ts
+++ http/index.ts
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000/api';
+export const STATIC_URL = 'http://localhost:5000';
+export const API_URL = `${STATIC_URL}/api`;
 
 const $api = axios.create({
   withCredentials: true,
