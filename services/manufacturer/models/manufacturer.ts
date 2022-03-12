90790db931eb40db36e9cd3b61d509ff5f7e6a7e
danillyamkin <danilwebdev@gmail.com>
2022-03-12 13:19:20 +0300

    Added logo

--- services/manufacturer/models/manufacturer.ts
+++ services/manufacturer/models/manufacturer.ts
@@ -3,6 +3,7 @@ import { Car } from '../../car/models/car';
 export interface Manufacturer {
   id: number;
   name: string;
+  logo: string;
   address: string;
   director: string;
   accountant: string;
