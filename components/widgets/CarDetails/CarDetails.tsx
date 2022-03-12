9d9225d6ead4a53e1210ac01ee39435c7d6d5105
danillyamkin <danilwebdev@gmail.com>
2022-03-12 13:22:15 +0300

    Created CarDetails component

---
+++ components/widgets/CarDetails/CarDetails.tsx
@@ -0,0 +1,58 @@
+import React, { FunctionComponent } from 'react';
+
+import classes from './CarDetails.module.scss';
+import { Car } from '../../../services/car/models/car';
+import { priceFormatter } from '../../../helpers/priceFormatter';
+import CarManufacturer from '../../ui/CarManufacturer/CarManufacturer';
+import { STATIC_URL } from '../../../http';
+
+interface CarDetailsProps {
+  car: Car;
+}
+
+type Props = CarDetailsProps;
+
+const CarDetails: FunctionComponent<Props> = ({ car }): JSX.Element => {
+  console.log(car);
+  return (
+    <div className={classes.layout}>
+      <section className={classes.content}>
+        <h2 className={classes.title}>What Is the 2021 BMW 2 Series?</h2>
+
+        <p className={classes.text}>
+          The coupe and convertible come in 230i and M240i form. The 230i is
+          powered by a 248-horsepower, turbocharged 2.0-liter four-cylinder
+          engine, while the M240i gets a 335-hp, turbocharged 3.0-liter
+          inline-six. Both versions are offered with rear- or all-wheel drive
+          and an eight-speed automatic or six-speed manual transmission.
+        </p>
+
+        <p className={classes.text}>
+          The Gran Coupe is powered by a turbocharged 2.0-liter four-cylinder
+          that makes 228 hp in the 228i and 301 hp in the M235i. The Gran Coupe
+          has standard all-wheel drive and an eight-speed automatic
+          transmission.
+        </p>
+      </section>
+
+      <aside className={classes.aside}>
+        <span className={classes.price}>
+          {priceFormatter.format(car.price)}
+
+          <div className={classes.manufacturer}>
+            <span className={classes.manufacturerDescription}>
+              Производитель
+            </span>
+
+            <CarManufacturer
+              logo={`${STATIC_URL}/${car.manufacturer.logo}`}
+              name={car.manufacturer.name}
+            />
+          </div>
+        </span>
+      </aside>
+    </div>
+  );
+};
+
+export default CarDetails;
