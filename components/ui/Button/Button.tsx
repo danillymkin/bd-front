13c4aa31275d18d99cbad84b0f6a3dbf650ec541
danillyamkin <danilwebdev@gmail.com>
2022-04-05 18:04:21 +0300

    Added icons, width, size props

--- components/ui/Button/Button.tsx
+++ components/ui/Button/Button.tsx
@@ -1,11 +1,16 @@
-import React, { FunctionComponent } from 'react';
+import React, { ButtonHTMLAttributes, FunctionComponent } from 'react';
 
 import classes from './Button.module.scss';
 import classNames from 'classnames/bind';
 
-interface ButtonProps {
+interface ButtonProps
+  extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'type'> {
   type?: 'default' | 'primary' | 'pagination';
   className?: string;
+  width?: 'default' | 'full';
+  size?: 's' | 'm';
+  leftIcon?: JSX.Element;
+  rightIcon?: JSX.Element;
 }
 
 type Props = ButtonProps;
@@ -16,16 +21,39 @@ const Button: FunctionComponent<Props> = ({
   children,
   className = '',
   type = 'default',
+  width = 'default',
+  size = 's',
+  leftIcon,
+  rightIcon,
 }): JSX.Element => {
   const buttonClasses = cx({
     button: true,
     primary: type === 'primary',
     default: type === 'default',
     pagination: type === 'pagination',
+    widthFull: width === 'full',
+    small: size === 's',
+    medium: size === 'm',
     [className]: className,
   });
 
-  return <button className={buttonClasses}>{children}</button>;
+  const leftIconClasses = cx({
+    icon: true,
+    iconLeft: true,
+  });
+
+  const rightIconClasses = cx({
+    icon: true,
+    iconRight: true,
+  });
+
+  return (
+    <button type={'button'} className={buttonClasses}>
+      {leftIcon && <div className={leftIconClasses}>{leftIcon}</div>}
+      {children}
+      {rightIcon && <div className={rightIconClasses}>{rightIcon}</div>}
+    </button>
+  );
 };
 
 export default Button;
